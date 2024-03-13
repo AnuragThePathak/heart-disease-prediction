a10250bf669f8ec0891faefa822dfdb0674efc4b
Anurag Pathak <anuragpathak911@gmail.com>
2024-03-13 11:09:57 +0530

    Refactor code to use a pipeline for preprocessing and model prediction

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
+import numpy as np
 import pandas as pd
 from pydantic import BaseModel
-import joblib
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+import joblib
+from sklearn.pipeline import Pipeline
 
 # Load your model here
 model = joblib.load('model/model.pkl')
 
-# Load the preprocessor
+# Define the categorical columns and the preprocessor
+categorical_columns = ['sex', 'cp','dataset','fbs','restecg','exang','slope']
 preprocessor = joblib.load('model/preprocessor.pkl')
+
+# Create a pipeline that first transforms the data and then applies the model
+pipeline = Pipeline(steps=[('preprocessor', preprocessor), ('model', model)])
 
 class Item(BaseModel):
     # Define your input schema here
@@ -27,46 +32,11 @@ class Item(BaseModel):
     exang: bool
     oldpeak: float
     slope: str
-    num: int
 
 app = FastAPI()
-
-# Load training data
-train_data = pd.read_csv('model/preprocessed.csv')
-
-# Identify numeric and categorical columns
-numeric_columns = ['age', 'trestbps', 'chol', 'thalch', 'oldpeak']
-categorical_columns = ['sex', 'cp', 'dataset', 'restecg', 'slope']
-
-# Initialize SimpleImputer for numeric features
-numeric_imputer = SimpleImputer(strategy='mean')
-
-# Initialize SimpleImputer for categorical features
-categorical_imputer = SimpleImputer(strategy='most_frequent')
-
-# Initialize OneHotEncoder for categorical features
-one_hot_encoder = OneHotEncoder()
-
-# Create a ColumnTransformer to apply different preprocessing steps to numeric and categorical columns
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('numeric', numeric_imputer, numeric_columns),
-        ('categorical', one_hot_encoder, categorical_columns)
-    ],
-    remainder='passthrough'
-)
-
-# Fit the ColumnTransformer with training data
-preprocessor.fit(train_data)
-
 @app.post("/predict")
 async def predict(item: Item):
-    # Convert item to a DataFrame
-    data = pd.DataFrame(item.dict(), index=[0])
-    
-    # Preprocess the input data
-    preprocessed_data = preprocessor.transform(data)
-    
-    # Predict using the model
-    prediction = model.predict(preprocessed_data)
-    return {"prediction": prediction.tolist()}
+    # Convert the item to a dictionary, get the values, convert to list and reshape it
+    data = pd.DataFrame([item.dict()])
+    prediction = pipeline.predict(data)
+    return {"prediction": prediction}
