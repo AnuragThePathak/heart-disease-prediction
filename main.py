e078a99b2cc6ae5ba4ddb5c70f75c25de9bc4ca7
Anurag Pathak <anuragpathak911@gmail.com>
2024-03-13 23:58:42 +0530

    Refactor input data conversion and preprocessing

--- main.py
+++ main.py
@@ -1,43 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import numpy as np
 import pandas as pd
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
 import joblib
-from sklearn.pipeline import Pipeline
 
 # Load your model and preprocessor
 model = joblib.load('model/model.pkl')
 scaler = joblib.load('model/scaler.pkl')
 
 # Define the input schema
-# class Item(BaseModel):
-#     id: int
-#     age: int
-#     sex: str
-#     dataset: str
-#     cp: str
-#     trestbps: float
-#     chol: float
-#     fbs: bool
-#     restecg: str
-#     thalch: float
-#     exang: bool
-#     oldpeak: float
-#     slope: str
-
 class Item(BaseModel):
     id: int
     age: int
-    sex: int
-    cp: int
+    sex: str
+    cp: str
     trestbps: float
     chol: float
-    fbs: int
-    restecg: int
+    fbs: bool
+    restecg: str
     thalch: float
-    exang: int
+    exang: bool
     oldpeak: float
 
 app = FastAPI()
@@ -45,15 +26,19 @@ app = FastAPI()
 @app.post("/predict")
 async def predict(item: Item):
     # Convert the input data into a DataFrame to work with ColumnTransformer
-    input_data = pd.DataFrame([item.dict()])
+    input_data = pd.DataFrame([item.model_dump()])
 
     # Ensure correct data types
     # This step is important because FastAPI converts all boolean values to True/False, but your model might expect 1/0
-    # input_data['fbs'] = input_data['fbs'].astype(int)
-    # input_data['exang'] = input_data['exang'].astype(int)
-    
+    input_data['fbs'] = input_data['fbs'].astype(int)
+    input_data['exang'] = input_data['exang'].astype(int)
+
+    # Convert string datatypes to integer
+    input_data['sex'] = input_data['sex'].map({'Male': 1, 'Female': 0})
+    input_data['cp'] = input_data['cp'].map({'asymptomatic': 0, 'non-anginal': 1, 'atypical-angina': 2, 'typical-angina': 3})
+    input_data['restecg'] = input_data['restecg'].map({'normal': 0, 'lv-hypertrophy': 1, 'st-t-abnormality': 2})
+
     # Preprocess the input data using the loaded preprocessor
-    print(input_data)
     preprocessed_data = scaler.transform(input_data)
 
     # Make a prediction
